d31b280c5b583b230700d9391c483fcafab59e59
Andrew Shields <andrewshields@Scarletts-MBP.attlocal.net>
2020-06-24 21:06:13 -0400

    minor style changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import Feed from "./userPage/Feed";
-// import Posted from "./userPage/Posted";
-// import Saved from "./userPage/Saved";
 
 export const Home = () => {
-	// return (
-	// 	<div className="homePage">
-	// 		<div className="leftCol">
-	// 			<h2>POSTED/SAVED:</h2>
-	// 			<hr />
-	// 			<Posted />
-	// 			<hr />
-	// 			<Saved />
-	// 		</div>
-	// 		<div className="rightCol">
-	// 			<h1>FEED:</h1>
-	// 			<Feed />
-	// 		</div>
-	// 	</div>
-	// );
 	return (
 		<div className="homePage">
 			<Feed />
